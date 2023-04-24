37f1f7ff4f9f69267835674c4bffac28fc9b3323
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-04-24 09:18:57 +0200

    run with sudo, cleanup processes

--- pull-data.sh
+++ pull-data.sh
@@ -2,13 +2,27 @@
 
 today=$( date +%Y-%m-%d )
 
+# cleanup background processes /bin/sh (dash) compatible
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
+
 lsusb | grep Garmin
-#dmesg -T -w &
-udevadm monitor -k
-python3 ./send_command.py 
+#test [ $( ps a | grep dmesg | wc -l ) -lt 2 ] || sudo dmesg -T -w &
+udevadm monitor -k &
+# add      /devices/pci0000:00/0000:00:14.0/usb1/1-1/1-1:1.0/host1/target1:0:0/1:0:0:0/block/sdb
+
+dev=/dev/sdb
+
+test -e $dev || sudo python3 ./send_command.py 
+
 test -e sdb || mkdir sdb
-mount /dev/sdb sdb/ -o ro
+sudo mount $dev sdb/ -o ro
+
+test -d out || mkdir out && git -C out init
+
 rsync -rav sdb/ out/
+git -C out add '*'
 git -C out commit -m $today -a
 
 #eject /dev/sdb
